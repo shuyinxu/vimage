673a5faa257d634d6ef6ae9b359eb57dd40ccaba
Shuyin Xu <xu.shuyin98@gmail.com>
2015-07-29 15:10:00 -0700

    added the colors

--- Canvas.js
+++ Canvas.js
@@ -20,6 +20,18 @@ function cBlue () {
 	pColor = [6,214,255]; 
 	return pColor;
 }
+function cPurple () {
+		pColor = [208,131,252]; 
+		return pColor;
+	}
+function cBrown () {
+	pColor = [190,117,47]; 
+	return pColor;
+}
+function cBlack () {
+	pColor = [0,0,0]; 
+	return pColor;
+	}
 function erase () {
 	pColor = [250,250,250]; 
 	return pColor;
@@ -42,3 +54,29 @@ function getTheme(){
 	var temp = subject[Math.floor(Math.random()*(subject.length))];
 	document.getElementById("theme").innerHTML = temp;
 }
+
+//timer
+var a = 30;
+var t;
+var timer_on = 0;
+var z = "Time is up"; 
+
+function count() {
+    document.getElementById("txt").value = a;
+    a = a - 1; 
+    t = setTimeout(function(){ count() }, 1000);
+	if (document.getElementById("txt").value < 1) {
+		document.getElementById("txt").value = z;
+	}
+}
+
+
+
+
+function startCount() {
+    if (!timer_on) {
+        timer_on = 1;
+        count()
+		
+		}
+    }
