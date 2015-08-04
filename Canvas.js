91af37714f93106bedd83c38cc1a66a6b8d29585
Jessica Nguyen <jessica.nguyen814@gmail.com>
2015-08-04 14:28:31 -0700

    FIXED BUG WHEN CLICK NEXT PLAYER

--- Canvas.js
+++ Canvas.js
@@ -116,53 +116,35 @@ function getSubjects () {
 }
 
 //TIMER
-var a = 20;
+var a = 5;
 var t;
 var timer_on = 0;
 var z = "TIME IS UP!"; 
 var t;
+var turn=1;
 
 function count() {
-	//do {
     document.getElementById("txt").value = a;
     a = a - 1; 
     t = setTimeout(function(){ count();setTimeout(reset, 1750); }, 1000);
 	if (document.getElementById("txt").value < 1) {
 		document.getElementById("txt").value = z;
 		alert("Next Player");
+		turn=0;
 		getTheme();
-		a = 20;
+		a = 5;
 	}
 }
 
 function startCount() {
     if (!timer_on ) {
         timer_on = 1;
-        count()
+        count();
 	}
 }
 
 var sTime = new Date().getTime();
 var countDown = 30;
-
-function UpdateTime() {
-    var cTime = new Date().getTime();
-    var diff = cTime - sTime;
-    var seconds = countDown - Math.floor(diff / 1000);
-    if (seconds >= 0) {
-        var minutes = Math.floor(seconds / 60);
-        seconds -= minutes * 60;
-        $("#minutes").text(minutes < 10 ? "0" + minutes : minutes);
-        $("#seconds").text(seconds < 10 ? "0" + seconds : seconds);
-    } else {
-        $("#countdown").hide();
-        $("#aftercount").show();
-        clearInterval(counter);
-    }
-}
-
-UpdateTime();
-var counter = setInterval(UpdateTime, 500);	
 
 function pauseTimer () {
 	alert ("Game Paused");
